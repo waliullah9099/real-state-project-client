59548675bce94e1b67913011b58ca41d7bea1315
MD Waliullah <waliullah9099@gmail.com>
2025-01-03 04:54:30 +0600

    protect route with second way

--- src/app/(withCommonLayout)/(home)/bookings/page.js
+++ src/app/(withCommonLayout)/(home)/bookings/page.js
@@ -1,25 +1,31 @@
 import ProfileInfo from "@/components/user/ProfileInfo";
 import PastBooking from "@/components/user/booking/PastBooking";
 import UpcomingBooking from "@/components/user/booking/UpcomingBooking";
+import { redirect } from "next/navigation";
+import { auth } from "../../../../../auth";
 
-const BookingsPage = () => {
-    return (
-        <>
-            <section className="mt-[100px]">
-                <div className="container">
-                    <ProfileInfo />
-                </div>
-            </section>
-            <section>
-                <div className="container">
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-                        <PastBooking />
-                        <UpcomingBooking />
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+const BookingsPage = async () => {
+  const session = await auth();
+  if (!session) {
+    redirect("/login");
+  }
+  return (
+    <>
+      <section className="mt-[100px]">
+        <div className="container">
+          <ProfileInfo />
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+            <PastBooking />
+            <UpcomingBooking />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default BookingsPage;
