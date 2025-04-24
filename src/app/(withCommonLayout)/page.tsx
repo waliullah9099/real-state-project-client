877d012c4868c102e4b352a47c553fc6a45c37a5
MD Waliullah <waliullah9099@gmail.com>
2025-04-24 13:43:32 +0600

    findout the issue why do not build

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,10 +1,10 @@
 import Hero from "@/components/Home/Hero";
 import OurBlogs from "@/components/Home/OurBlogs/OurBlogs";
+import OurAgent from "@/components/Home/OurAgents/OurOgents";
 import OurLocation from "@/components/Home/OurLocation/OurLocation";
 import OurServices from "@/components/Home/OurServices/OurServices";
 import WhyChooseUs from "@/components/Home/WhyChooseUs/WhyChooseUs";
 import FeaturedProperties from "../../components/Home/FeaturedProperties/FeaturedProperties";
-import OurAgent from "@/components/Home/OurAgents/OurOgents";
 
 export default function Home() {
   return (
--- src/app/api/hotels/route.js
+++
@@ -1,7 +0,0 @@
-import { hotelModel } from "../../../models/hotel-model";
-import { replaceMongoIdInArray } from "../../../utils/data-utils";
-
-export async function getAllHotels() {
-  const hotels = await hotelModel.find().lean();
-  return replaceMongoIdInArray(hotels);
-}
