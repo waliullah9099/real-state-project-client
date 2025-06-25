1ec41164d17b0e6827f60d5af6b39253d1075451
MD Waliullah <waliullah9099@gmail.com>
2025-06-25 17:42:40 +0600

    upload properties json data

--- public/data/properties.ts
+++ public/data/properties.ts
@@ -90,3 +90,322 @@ export const properties = [
       "https://homelengohtml.vercel.app/images/avatar/avt-png3.png",
   },
 ];
+
+
+
+export const dashboardProperties = [
+  {
+    _id: "68090405978cb8298eb63819",
+    user: "6809039e978cb8298eb6380d",
+    title: "key",
+    description: "Stunning modern villa with a private pool and panoramic city views.",
+    price: 2500000,
+    images: [
+      {
+        "thumbNailImage": "https://i.ibb.co.com/nD1gkWX/location-2.jpg"
+      },
+      {}
+    ],
+    type: "House",
+    propertyFor: "For Sale",
+    bedrooms: 5,
+    bathrooms: 4,
+    city: "Los Angeles",
+    country: "USA",
+    propertyAddress: "456 Beverly Hills, CA 90210",
+    squareFoot: 5000,
+    isFeatured: true,
+    buildYear: "2019",
+    status: "pending",
+    createdAt: "2025-04-23T15:15:17.738+00:00",
+    updatedAt: "2025-05-01T12:05:25.021+00:00",
+    __v: 0
+  },
+  {
+    _id: "680952e7e08cf68537029b1f",
+    user: "6809039e978cb8298eb6380d",
+    title: "private pool and panoramic city",
+    description: "Stunning modern villa with a private pool and panoramic city views.",
+    price: 2500,
+    images: [
+      {
+        "thumbNailImage": "https://i.ibb.co.com/nD1gkWX/location-2.jpg"
+      },
+      {}
+    ],
+    type: "Apartment",
+    propertyFor: "For Rent",
+    bedrooms: 5,
+    bathrooms: 4,
+    city: "Los Angeles",
+    country: "USA",
+    propertyAddress: "456 Beverly Hills, CA 90210",
+    squareFoot: 5000,
+    isFeatured: true,
+    buildYear: "2019",
+    status: "pending",
+    createdAt: "2025-04-23T20:51:51.650+00:00",
+    updatedAt: "2025-04-23T20:51:51.650+00:00",
+    __v: 0
+  },
+  {
+    _id: "68095310e08cf68537029b25",
+    user: "6809039e978cb8298eb6380d",
+    title: "private pool and panoramic city",
+    description: "Stunning modern villa with a private pool and panoramic city views.",
+    price: 2500,
+    images: [
+      {
+        "thumbNailImage": "https://i.ibb.co.com/nD1gkWX/location-2.jpg"
+      },
+      {}
+    ],
+    type: "Apartment",
+    propertyFor: "Sold",
+    bedrooms: 5,
+    bathrooms: 4,
+    city: "Los Angeles",
+    country: "USA",
+    propertyAddress: "456 Beverly Hills, CA 90210",
+    squareFoot: 5000,
+    isFeatured: true,
+    buildYear: "2019",
+    status: "pending",
+    createdAt: "2025-04-23T20:52:32.777+00:00",
+    updatedAt: "2025-04-23T20:52:32.777+00:00",
+    __v: 0
+  },
+  {
+    _id: "6809b67dcb5e2ab8f8de304a",
+    user: "6809039e978cb8298eb6380d",
+    title: "private pool and panoramic city",
+    description: "Stunning modern villa with a private pool and panoramic city views.",
+    price: 2500,
+    images: [
+      {
+        "thumbNailImage": "https://i.ibb.co.com/nD1gkWX/location-2.jpg"
+      },
+      {}
+    ],
+    type: "Apartment",
+    propertyFor: "Sold",
+    bedrooms: 5,
+    bathrooms: 4,
+    city: "Los Angeles",
+    country: "USA",
+    propertyAddress: "456 Beverly Hills, CA 90210",
+    squareFoot: 5000,
+    isFeatured: true,
+    buildYear: "2019",
+    status: "pending",
+    createdAt: "2025-04-24T03:56:45.760+00:00",
+    updatedAt: "2025-04-24T03:56:45.760+00:00",
+    __v: 0
+  },
+  {
+    _id: "6809b682cb5e2ab8f8de304f",
+    user: "6809039e978cb8298eb6380d",
+    title: "private pool and panoramic city",
+    description: "Stunning modern villa with a private pool and panoramic city views.",
+    price: 2500,
+    images: [
+      {
+        "thumbNailImage": "https://i.ibb.co.com/nD1gkWX/location-2.jpg"
+      },
+      {}
+    ],
+    type: "Apartment",
+    propertyFor: "Sold",
+    bedrooms: 5,
+    bathrooms: 4,
+    city: "Los Angeles",
+    country: "USA",
+    propertyAddress: "456 Beverly Hills, CA 90210",
+    squareFoot: 5000,
+    isFeatured: true,
+    buildYear: "2019",
+    status: "pending",
+    createdAt: "2025-04-24T03:56:50.815+00:00",
+    updatedAt: "2025-04-24T03:56:50.815+00:00",
+    __v: 0
+  },
+  {
+    _id: "6809b686cb5e2ab8f8de3054",
+    user: "6809039e978cb8298eb6380d",
+    title: "private pool and panoramic city",
+    description: "Stunning modern villa with a private pool and panoramic city views.",
+    price: 2500,
+    images: [
+      {
+        "thumbNailImage": "https://i.ibb.co.com/nD1gkWX/location-2.jpg"
+      },
+      {}
+    ],
+    type: "Apartment",
+    propertyFor: "Sold",
+    bedrooms: 5,
+    bathrooms: 4,
+    city: "Los Angeles",
+    country: "USA",
+    propertyAddress: "456 Beverly Hills, CA 90210",
+    squareFoot: 5000,
+    isFeatured: true,
+    buildYear: "2019",
+    status: "pending",
+    createdAt: "2025-04-24T03:56:54.090+00:00",
+    updatedAt: "2025-04-24T03:56:54.090+00:00",
+    __v: 0
+  },
+  {
+    _id: "6809b688cb5e2ab8f8de3059",
+    user: "6809039e978cb8298eb6380d",
+    title: "private pool and panoramic city",
+    description: "Stunning modern villa with a private pool and panoramic city views.",
+    price: 2500,
+    images: [
+      {
+        "thumbNailImage": "https://i.ibb.co.com/nD1gkWX/location-2.jpg"
+      },
+      {}
+    ],
+    type: "Apartment",
+    propertyFor: "Sold",
+    bedrooms: 5,
+    bathrooms: 4,
+    city: "Los Angeles",
+    country: "USA",
+    propertyAddress: "456 Beverly Hills, CA 90210",
+    squareFoot: 5000,
+    isFeatured: true,
+    buildYear: "2019",
+    status: "pending",
+    createdAt: "2025-04-24T03:56:56.719+00:00",
+    updatedAt: "2025-04-24T03:56:56.719+00:00",
+    __v: 0
+  },
+  {
+    _id: "6809b68acb5e2ab8f8de305e",
+    user: "6809039e978cb8298eb6380d",
+    title: "private pool and panoramic city",
+    description: "Stunning modern villa with a private pool and panoramic city views.",
+    price: 2500,
+    images: [
+      {
+        "thumbNailImage": "https://i.ibb.co.com/nD1gkWX/location-2.jpg"
+      },
+      {}
+    ],
+    type: "Apartment",
+    propertyFor: "Sold",
+    bedrooms: 5,
+    bathrooms: 4,
+    city: "Los Angeles",
+    country: "USA",
+    propertyAddress: "456 Beverly Hills, CA 90210",
+    squareFoot: 5000,
+    isFeatured: true,
+    buildYear: "2019",
+    status: "pending",
+    createdAt: "2025-04-24T03:56:58.767+00:00",
+    updatedAt: "2025-04-24T03:56:58.767+00:00",
+    __v: 0
+  },
+  {
+    _id: "681312dbbf4716c360f9c8dd",
+    user: "680902c185874e9b52b577d4",
+    title: "private pool and panoramic city",
+    description: "Stunning modern villa with a private pool and panoramic city views.",
+    price: 2500,
+    images: [
+      {
+        "thumbNailImage": "https://i.ibb.co.com/nD1gkWX/location-2.jpg"
+      },
+      {}
+    ],
+    type: "Apartment",
+    propertyFor: "Sold",
+    bedrooms: 5,
+    bathrooms: 4,
+    city: "Los Angeles",
+    country: "USA",
+    propertyAddress: "456 Beverly Hills, CA 90210",
+    squareFoot: 5000,
+    isFeatured: true,
+    buildYear: "2019",
+    status: "approved",
+    createdAt: "2025-05-01T06:21:15.567+00:00",
+    updatedAt: "2025-05-01T06:21:15.567+00:00",
+    __v: 0
+  },
+  {
+    _id: "681312e7bf4716c360f9c8e2",
+    user: "680902c185874e9b52b577d4",
+    title: "private pool and panoramic city",
+    description: "Stunning modern villa with a private pool and panoramic city views.",
+    price: 2500,
+    images: [
+      {
+        "thumbNailImage": "https://i.ibb.co.com/nD1gkWX/location-2.jpg"
+      },
+      {}
+    ],
+    type: "Apartment",
+    propertyFor: "Sold",
+    bedrooms: 5,
+    bathrooms: 4,
+    city: "Los Angeles",
+    country: "USA",
+    propertyAddress: "456 Beverly Hills, CA 90210",
+    squareFoot: 5000,
+    isFeatured: true,
+    buildYear: "2019",
+    status: "approved",
+    createdAt: "2025-05-01T06:21:27.562+00:00",
+    updatedAt: "2025-05-01T06:21:27.562+00:00",
+    __v: 0
+  },
+  {
+    _id: "681315f6bf4716c360f9c8f3",
+    user: "680902c185874e9b52b577d4",
+    title: "Tahar villa",
+    description: "Now we can use the React-Redux hooks to let React comp. ",
+    price: 100,
+    images: [],
+    "thumbNailImage": "https://real-estate-snowy-five.vercel.app/_next/image?url=%2F_next%2Fs…",
+    "type": "Apartment",
+    "propertyFor": "For Rent",
+    "bedrooms": 2,
+    "bathrooms": 4,
+    city: "Satkhira",
+   country: "Bangladsh",
+    propertyAddress: "ala",
+    squareFoot: 11,
+    isFeatured: false,    buildYear: "2003",
+    staus: "approved",
+   createdAt: "2025-05-01T06:34:30.741+00:00",
+  updatedAt: "2025-05-01T06:34:30.741+00:00",
+  __v: 0
+ },
+  {    _id:"68131621bf4716c360f9c8f8",
+  user: "68902c185874e9b52b577d4",
+   tile: "Tahar villa",
+    description: "Now we can use the React-Redux hooks to let React comp. ",
+    price: 100,
+    images: [],
+    thumbNailImage: "https://real-estate-snowy-five.vercel.app/_next/image?url=%2F_next%2Fs…",
+    type: "Apartment",
+    propertyFor: "For Rent",
+    bedrooms: 2,
+    bathrooms: 4,
+   city: "Satkhira",
+    country: "Bangladeh",
+    propertyAddres: "Tala",
+    squareFoot: 121,    isFeatured: false,
+    buidYear: "2003",
+   status: "approved",
+  createdAt: "2025-05-01T06:35:13.279+00:00",
+   updatedAt: "2025-05-01T06:35:13.279+00:00",   
+   __v: 0
+
+  }
+]
